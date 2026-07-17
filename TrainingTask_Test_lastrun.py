75a00eead14ef472f1a2eb99aa61f493082083e1
Maria S. Moreira <mariasilveiramoreira04@gmail.com>
2026-07-17 16:22:35 +0100

    Added explanation for the task in portuguese in the text component, added a 4000ms pause before the task

--- TrainingTask_Test_lastrun.py
+++ TrainingTask_Test_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2026.1.2),
-    on julho 17, 2026, at 16:14
+    on julho 17, 2026, at 16:17
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
