403763f81dc857249926a71de3e687cddcdb6997
Maria S. Moreira <mariasilveiramoreira04@gmail.com>
2026-07-21 13:05:21 +0100

    added the other parameters

--- TrainingTask_Test_lastrun.py
+++ TrainingTask_Test_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2026.1.2),
-    on julho 20, 2026, at 15:15
+    on julho 21, 2026, at 12:58
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -10,7 +10,7 @@ If you publish work using this script the most relevant publication is:
         https://doi.org/10.3758/s13428-018-01193-y
 
 """
-# teste
+
 # --- Import packages ---
 from psychopy import locale_setup
 from psychopy import prefs
@@ -132,7 +132,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version=expVersion,
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='C:\\Users\\Asus\\Documents\\recursos\\facul4.2\\Tese\\TrainingTask_Test\\TrainingTask_Test_lastrun.py',
+        originPath='C:\\Users\\Asus\\Documents\\recursos\\H-IBL.Task\\TrainingTask_Test_lastrun.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
@@ -201,7 +201,7 @@ def setupWindow(expInfo=None, win=None):
     if win is None:
         # if not given a window to setup, make one
         win = visual.Window(
-            size=_winSize, fullscr=_fullScr, screen=0,
+            size=_winSize, fullscr=_fullScr, screen=1,
             winType='pyglet', allowGUI=False, allowStencil=False,
             monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
             backgroundImage='', backgroundFit='none',
@@ -412,8 +412,8 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
         opacity=None, depth=-1.0, interpolate=True)
     grating100or50 = visual.GratingStim(
         win=win, name='grating100or50',units='deg', 
-        tex='sin', mask='gauss', anchor='center',
-        ori=0.0, pos=[0,0], draggable=False, size=1.0, sf=1.0, phase=5.0,
+        tex=None, mask='gauss', anchor='center',
+        ori=1.0, pos=[0,0], draggable=False, size=1.0, sf=1.0, phase=1.0,
         color=[1,1,1], colorSpace='rgb',
         opacity=None, contrast=1.0, blendmode='avg',
         texRes=256.0, interpolate=True, depth=-2.0)
@@ -906,6 +906,10 @@ def run(expInfo, thisExp, win, globalClock=None, thisSession=None):
                 # update params
                 grating100or50.setPos(position, log=False)
                 grating100or50.setSize(size, log=False)
+                grating100or50.setOri(orientation, log=False)
+                grating100or50.setTex(texture, log=False)
+                grating100or50.setSF(spacial_freq, log=False)
+                grating100or50.setPhase(phase, log=False)
             
             # if grating100or50 is stopping this frame...
             if grating100or50.status == STARTED:
